e973d5245aa96a108056f457c61f3312b3e24ab6
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-04-22 20:25:38 -0300

    casa de cambio

---
+++ src/main.js
@@ -0,0 +1,73 @@
+// import Swal from 'sweetalert2';
+// import '@sweetalert2/theme-dark/dark.css';
+
+const searchButton = document.getElementById('search-button');
+const currencyField = document.getElementById('currency');
+const currencySection = document.getElementById('currency-section');
+const BASE_URL = 'https://api.exchangerate.host/latest?base=';
+
+const createElements = (item) => {
+  const tableSection = document.getElementsByClassName('tableSection')[0];
+
+  const coinBase = document.createElement('div');
+  coinBase.className = 'coinBase';
+  tableSection.appendChild(coinBase);
+
+  const coin = document.createElement('img');
+  coin.setAttribute('src', './src/icon/coins.svg');
+  coinBase.appendChild(coin);
+
+  const [first, second] = item;
+  const coinName = document.createElement('p');
+  coinName.innerHTML = first;
+  coinBase.appendChild(coinName);
+
+  const coinValue = document.createElement('p');
+  coinValue.innerHTML = second.toFixed(3);
+  coinBase.appendChild(coinValue);
+};
+
+searchButton.addEventListener('click', (event) => {
+  currencySection.innerHTML = '';
+  event.preventDefault();
+
+  const currencyName = currencyField.value.toUpperCase();
+
+  if (!currencyName) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Você precisa passar uma moeda',
+    });
+    return;
+  }
+
+  const currencyURL = `${BASE_URL}${currencyName}`;
+  fetch(currencyURL)
+    .then((response) => response.json())
+    .then(({ rates }) => {
+      const data = Object.entries(rates);
+
+      if (!data.some((element) => element.includes(currencyName))) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Moeda não existente!',
+        });
+        return;
+      }
+
+      const sectionTitle = document.createElement('h2');
+      sectionTitle.innerHTML = `Valores referentes a 1 ${currencyName}`;
+      sectionTitle.className = 'section-title';
+      currencySection.appendChild(sectionTitle);
+
+      const tableSection = document.createElement('div');
+      tableSection.className = 'tableSection';
+      currencySection.appendChild(tableSection);
+
+      data.forEach((item) => {
+        createElements(item);
+      });
+    });
+});
